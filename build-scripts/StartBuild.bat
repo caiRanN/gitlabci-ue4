01641488b5f896b70ecac38adb72614631bb193e
Cairan Steverink <cairan_432@hotmail.com>
2018-02-27 19:29:01 +0100

    Update StartBuild.bat

--- build-scripts/StartBuild.bat
+++ build-scripts/StartBuild.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Epic Games\UE_4.18\Engine\Binaries\DotNET\UnrealBuildTool.exe" -projectfiles -project="%CI_PROJECT_DIR%\PROJECT_NAME.uproject" -game -rocket -progress
+"C:\PATH_TO_ENGINE\UE_4.18\Engine\Binaries\DotNET\UnrealBuildTool.exe" -projectfiles -project="%CI_PROJECT_DIR%\PROJECT_NAME.uproject" -game -rocket -progress
