00a27082e2b2626f6e7b095751f92e73583ece16
Cairan Steverink <cairan_432@hotmail.com>
2018-02-27 19:29:12 +0100

    Update CookProject.bat

--- build-scripts/CookProject.bat
+++ build-scripts/CookProject.bat
@@ -1,1 +1,1 @@
-"C:\PATH_TO_ENGINE\Epic Games\UE_4.18\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="%CI_PROJECT_DIR%\PROJECT_NAME.uproject" -noP4 -platform=Win64 -clientconfig=Development -cook -allmaps -NoCompile -stage -pak -archive -archivedirectory="%CI_PROJECT_DIR%\Release\PROJECT_NAME"
+"C:\PATH_TO_ENGINE\UE_4.18\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="%CI_PROJECT_DIR%\PROJECT_NAME.uproject" -noP4 -platform=Win64 -clientconfig=Development -cook -allmaps -NoCompile -stage -pak -archive -archivedirectory="%CI_PROJECT_DIR%\Release\PROJECT_NAME"
